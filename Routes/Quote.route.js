bd827d2af06d426d9ad4bcec59641a8adcab897c
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-01-29 00:43:31 +0530

    backend and quotes

--- Routes/Quote.route.js
+++ Routes/Quote.route.js
@@ -6,8 +6,8 @@ const QuoteRouter = express.Router();
 QuoteRouter.get("/",async(req,res)=>{
     try {
         let data = await QuoteModel.aggregate(
-            [ { $sample: { size: 5 } } ]
-         ).limit(5);
+            [ { $sample: { size: 10 } } ]
+         ).limit(10);
         res.status(200).send({msg:"success",data})
     } catch (error) {
         res.status(400).send({msg:"oops something went wrong",error})
